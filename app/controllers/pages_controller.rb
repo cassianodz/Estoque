e2f2ced1a4df4fb0a37344cb475919d9d1a694ec
cassiano zago <cassianodz@gmail.com>
2022-04-26 19:47:10 -0400

    refatoracao consulta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: []
 
   def home
-
-    unless params[:search].nil?
-      unless params[:search][:escolha_radio].nil?
-        case params[:search][:escolha_radio]
+  binding.pry
+    if not params[:search].nil?
+      query = params[:search][:busca].downcase
+      @materials = Material.where('lower(descricao) like ? OR lower(lote) like ? OR lower(observacao) like ? AND quantidade > 0', "%#{query}%", "%#{query}%", "%#{query}%")
+      @materials = @materials.sort_by(&:vencimento)
+    elsif not params[:filter].nil?
+      case params[:filter]
         when "M"
           @materials = Material.where("area = 'Médico-Hospitalar'", "quantidade > 0" )
         when "O"
@@ -14,31 +17,9 @@ class PagesController < ApplicationController
           @materials = Material.where("area= 'Farmácia'", "quantidade > 0" )
         end
         @materials =  @materials.sort_by(&:vencimento)
-        return @materials
-      end
-      query = params[:search][:busca].downcase
-      @materials_copy = Material.where('lower(descricao) like ? OR lower(lote) like ? OR lower(observacao) like ? AND quantidade > 0', "%#{query}%", "%#{query}%", "%#{query}%")
-    end
-    select_dataset
-  end
-
-
-  private
-
-  def select_dataset
-    # TODO: Future update will store search params and it will be necessary to
-    # sort between search and sort params
-    if params[:sort].nil? && !params[:search].nil?
-      @materials = @materials_copy.order(:vencimento)
-    elsif params[:sort].nil? && params[:search].nil?
-      @materials = Material.where('quantidade > 0')
-      @materials = @materials.sort_by(&:vencimento)
-    elsif !params[:sort].nil? && params[:search].nil?
+    elsif not params[:sort].nil?
       @materials = Material.where('quantidade > 0')
       @materials = @materials.order(params[:sort])
-    elsif !params[:sort].nil? && !params[:search].nil?
-      @materials = @materials_copy.order(params[:sort])
     end
-    return @materials
   end
 end
